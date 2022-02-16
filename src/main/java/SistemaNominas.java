aad526ab6fadfd9af98136045fac052e41f9e248
Ricardo <ricardo.santom@educa.jcyl.es>
2022-02-16 11:37:51 +0100

    Update SistemaNominas.java

--- src/main/java/SistemaNominas.java
+++ src/main/java/SistemaNominas.java
@@ -2,10 +2,8 @@
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.TreeMap;
 
 /**
